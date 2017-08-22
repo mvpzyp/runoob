4e5825e8c7f2b4679cec0ac52d22001ce799905a
zyp <zyp@abssoft.com.cn>
2017-08-22 21:08:24 +0800

    20170822001

--- bin/buffer91.js
+++ bin/buffer91.js
@@ -26,3 +26,29 @@ encoding - 使用的编码。默认为 'utf8' 。
 var buf = new Buffer(256);
 var len = buf.write('www.runoob.com');
 console.log("写入字节数：" + len);
+/*
+读取 Node 缓冲区数据的语法如下所示：
+buf.toString([encoding[, start[, end]]])
+参数
+参数描述如下：
+encoding - 使用的编码。默认为 'utf8' 。
+start - 指定开始读取的索引位置，默认为 0。
+end - 结束位置，默认为缓冲区的末尾。
+返回值
+解码缓冲区数据并使用指定的编码返回字符串。*/
+buf = new Buffer(26);
+for(var i = 0 ; i < 26 ; i++){
+    buf[i] = i + 97;
+}
+console.log(buf.toString('ascii'));
+console.log(buf.toString('ascii',0,5));
+console.log(buf.toString('utf8',0,5));
+console.log(buf.toString(undefined,0,5));
+/*
+将 Node Buffer 转换为 JSON 对象的函数语法格式如下：
+buf.toJSON()
+返回值
+返回 JSON 对象。*/
+var buf = new Buffer('www.runoob.com');
+var json = buf.toJSON();
+ console.log(json);
